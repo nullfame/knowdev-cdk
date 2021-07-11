6fc8f0e2ce240f90464b6f8c1b5d3774634cb6eb
Adam Finlayson <amf@adamfinlayson.com>
2021-07-10 23:16:14 -0500

    feat: instantiate and tag app

--- src/core.js
+++ src/core.js
@@ -1,5 +1,21 @@
+const cdk = require("@aws-cdk/core");
+
 const { version } = require("../package.json");
 const Tagger = require("./util/Tagger");
+
+//
+//
+// Setup
+//
+
+// Declare
+const app = new cdk.App();
+const tagger = new Tagger();
+
+// Tag
+const now = new Date();
+cdk.Tags.of(app).add("buildTime", String(now.getTime()));
+cdk.Tags.of(app).add("knowDevCdk", version);
 
 //
 //
@@ -7,6 +23,7 @@ const Tagger = require("./util/Tagger");
 //
 
 module.exports = {
-  tagger: new Tagger(),
+  app,
+  tagger,
   VERSION: version,
 };
